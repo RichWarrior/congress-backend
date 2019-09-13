1d8a4c0d8743e9e3e4ca315f9b4edce4ac49386b
unknown <faruk_thecno@hotmail.com>
2019-09-13 20:21:43 +0300

    first commit

---
+++ Congress.PushNotification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Congress.PushNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
